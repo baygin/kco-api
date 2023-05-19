cd907490d4c1a2902b202abc01f98f0cb11de18d
baygin <info@onderbaygin.com>
2023-05-20 01:39:55 +0300

    çeviri durumu sayılaması (enumaration)

--- src/interfaces/answers.interface.ts
+++ src/interfaces/answers.interface.ts
@@ -1,10 +1,16 @@
 import { ObjectId } from 'mongoose';
 
+export enum AnswerStatus {
+  NotChecked = 0,
+  Unknown = 1,
+  True = 2,
+  False = 3,
+}
+
 export interface Answer {
   _id?: ObjectId;
   roundId: ObjectId;
-  userId?: ObjectId;
   answer: string;
   date?: Date;
-  isCorrect?: number;
+  isCorrect: AnswerStatus;
 }
